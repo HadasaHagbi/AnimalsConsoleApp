aa988a6c4b0e4e75364a498f06eed339de76b7cb
HadasaHagbi <134325069+HadasaHagbi@users.noreply.github.com>
2023-05-22 22:01:45 +0300

    Add files via upload

---
+++ ConsoleAppAnimalsHadasaHagbi/ConsoleAppAnimalsHadasaHagbi/Classes/Frog.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleAppAnimalsHadasaHagbi.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAnimalsHadasaHagbi.Classes
+{
+    internal class Frog : Animal, Water, Land
+    {
+        private int NumberOfLegs { get; set; }
+        private readonly string[] MOODS = { "make a purr, purr sound", "make a hiss sound" };
+
+        public Frog(bool mammals, bool carnivorous, int mood) : base(mammals, carnivorous, mood)
+        {
+
+        }
+        public override void sayHello(int mood)
+        {
+            string moodk = "";
+            this.mood = mood;
+            if (mood == 1)
+            {
+                moodk = "happy";
+            }
+            else
+            {
+                moodk = "scare";
+            }
+            Console.WriteLine("now i " + this.MOODS[mood - 1] + " because i " + moodk);
+        }
+        public int getNumberOfLegs()
+        {
+            return NumberOfLegs;
+        }
+
+        public bool hasGill()
+        {
+            return true;
+        }
+
+        public bool hasLaysEggs()
+        {
+            return true;
+        }
+
+
+
+    }
+}
---
+++ ConsoleAppAnimalsHadasaHagbi/ConsoleAppAnimalsHadasaHagbi/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppAnimalsHadasaHagbi.Classes;
+
+Console.WriteLine("I: Hello, Dog!");
+Dog d = new Dog(true, true, 2);
+Console.Write("Dog: ");
+d.sayHello();
+d.sayHello(1);
+d.setMammals(true);
+Console.WriteLine("isCarnivorous: " + d.isCarnivorous());
+Console.WriteLine("isMammals: " + d.isMammals());
+Console.WriteLine("==================");
+
+Cat c=new Cat(true, true, 2);
+Console.WriteLine("Hello, Cat!");
+Console.Write("Cat: ");
+c.sayHello();
+c.sayHello(1);
+c.sayHello(2);
+c.setMammals(true);
+Console.WriteLine("isCarnivorous: " + c.isCarnivorous());
+Console.WriteLine("isMammals: " + c.isMammals());
+Console.WriteLine("==================");
